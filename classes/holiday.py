6dbf185693aeeca6c5ab4f5a0873ea4ac8b498d4
nguyenchanhlong <chanhlong171002@gmail.com>
2023-05-20 13:45:23 +0700

    Update

--- classes/holiday.py
+++ classes/holiday.py
@@ -1,5 +1,14 @@
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
+import json
+from datetime import date
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, date):
+            return o.isoformat()
+        return super().default(o)
 
 
 class Holiday(Resource):
@@ -10,34 +19,37 @@ class Holiday(Resource):
         if request.query_string is not None or request.query_string != "":
             with self.connection.cursor() as cursor:
                 # get all
-                if request.args['jid'] == "*":
+                if request.args.get('jid') == "*":
                     drive = []
                     sql = "SELECT * FROM `tbl_holiday`"
                     cursor.execute(sql)
                     result = cursor.fetchall()
                     for i in result:
                         data = {
-                            'jid': (i[1]),
-                            'holiday_date': i[0].strftime("%m-%d-%Y")
+                            'jid': i[1],
+                            'holiday_date': i[0]
                         }
                         drive.append(data)
-                    return drive, 200
+                    return jsonify(drive)
+                    # or return drive, 200
 
                 # get by id
                 else:
                     sql = "SELECT * FROM `tbl_holiday` WHERE `jid`=%s"
                     drive = []
-                    cursor.execute(sql, (request.args['jid']))
+                    cursor.execute(sql, (request.args['jid'],))
                     result = cursor.fetchall()
                     for i in result:
                         data = {
-                            'jid': i[0],
-                            'holiday_date': i[1]
+                            'jid': i[1],
+                            'holiday_date': i[0]
                         }
                         drive.append(data)
-                    return drive, 200
+                    return jsonify(drive)
+                    # or return drive, 200
         else:
-            return {"status":"error"}
+            return jsonify({"status": "error"})
+            # or return {"status": "error"}, 200
 
     def post(self):
         if request.is_json:
@@ -49,9 +61,11 @@ class Holiday(Resource):
                 sql_post = sql_post.format(data['jid'], data['holiday_date'])
                 cursor.execute(sql_post)
                 self.connection.commit()
-            return {'status':'success'}, 201
+            return jsonify({'status': 'success'})
+            # or return {'status': 'success'}, 201
         else:
-            return {"status":"error"}
+            return jsonify({"status": "error"})
+            # or return {"status": "error"}, 200
 
     def delete(self):
         if request.is_json:
@@ -61,12 +75,16 @@ class Holiday(Resource):
             holiday_date = data['holiday_date']
             with self.connection.cursor() as cursor:
                 sql_delete = "DELETE FROM `tbl_holiday` WHERE `jid`='{}' and `holiday_date`='{}'"
-                sql_delete = sql_delete.format(jid,holiday_date)
+                sql_delete = sql_delete.format(jid, holiday_date)
                 cursor.execute(sql_delete)
+                # the connection is not autocommited by default. So we must commit to save our changes.
                 self.connection.commit()
-            return {"status": "success"}, 200
+            return jsonify({"status": "success"})
+            # or return {"status": "success"}, 200
         else:
-            return {"status":"error"}
+            return jsonify({"status": "error"})
+            # or return {"status": "error"}, 200
 
     def put(self):
-        return {"status":"method not supported"}
+        return jsonify({"status": "method not supported"})
+        # or return {"status": "method
