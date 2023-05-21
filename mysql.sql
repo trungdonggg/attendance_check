6172f2be87db958ea3831512c10a6f949c7b9af3
nguyenchanhlong <chanhlong171002@gmail.com>
2023-05-22 02:14:07 +0700

    update

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,5 @@
+CREATE SCHEMA `Attendance_Check_Databases` ;
+use Attendance_Check_Databases;
 
 CREATE TABLE `tbl_employee` (
   `eid` varchar(10) NOT NULL,
@@ -5,7 +7,7 @@ CREATE TABLE `tbl_employee` (
   `phone` varchar(13) NOT NULL,
   `email` varchar(40) NOT NULL,
   PRIMARY KEY (`eid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `tbl_job` (
   `jid` varchar(10) NOT NULL,
@@ -16,27 +18,27 @@ CREATE TABLE `tbl_job` (
   `late_coefficient` float NOT NULL,
   `overtime_coefficient` float NOT NULL,
   PRIMARY KEY (`jid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
  
 CREATE TABLE `tbl_attendance` (
   `clock_in` datetime NOT NULL,
   `clock_out` datetime DEFAULT NULL,
   `eid` varchar(10) NOT NULL,
   PRIMARY KEY (`clock_in`,`eid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `tbl_position` (
   `from_date` date NOT NULL,
   `eid` varchar(10) NOT NULL,
   `jid` varchar(10) NOT NULL,
   PRIMARY KEY (`from_date`,`eid`,`jid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `tbl_holiday` (
   `holiday_date` date NOT NULL,
   `jid` varchar(10) NOT NULL,
   PRIMARY KEY (`holiday_date`,`jid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE tbl_payment(
 eid VARCHAR(10) NOT NULL ,
